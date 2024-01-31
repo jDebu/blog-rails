01b13006f63d4e3efd226aa0e6792f0485592b88
manuel delgado <mdelgado.pcm@gmail.com>
2024-01-30 23:31:13 -0500

    fix: improve the spaces with justify content

--- app/frontend/routers/AppRouter.jsx
+++ app/frontend/routers/AppRouter.jsx
@@ -1,61 +1,76 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Header } from '../components/ui/Header'
-import { PublicRoute } from './PublicRoute'
-import '../assets/stylesheets/application.scss'
-import HomeRoutes from './HomeRouters'
-import Profile from '../assets/images/profile.svg'
-import Location from '../assets/icons/location.svg'
-import Github from '../assets/icons/github.svg'
-import Linkedin from '../assets/icons/linkedin.svg'
-import { Avatar } from '@mui/material'
-import { Container } from '../components/Container'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "../components/ui/Header";
+import { PublicRoute } from "./PublicRoute";
+import "../assets/stylesheets/application.scss";
+import HomeRoutes from "./HomeRouters";
+import Profile from "../assets/images/profile.svg";
+import Location from "../assets/icons/location.svg";
+import Github from "../assets/icons/github.svg";
+import Linkedin from "../assets/icons/linkedin.svg";
+import { Avatar } from "@mui/material";
+import { Container } from "../components/Container";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <Header />
-    <div className='flex'>
-    <main className="mt-4 flex-auto">
-      <Container className="px-5 md:px-20 flex items-center">
-        <div className='profile-header py-3'>
-          <h1 className="font-black">José Delgado</h1>
-        </div>
-      </Container>
-      <Routes>
-        <Route
-            path="/*"
-            element={
-              <PublicRoute redirect={'/'}>
-                <HomeRoutes />
-              </PublicRoute>
-            }
-          />
-      </Routes>
-    </main>
-    <aside className='mt-4 ml-2'>
-      <div>
-        <div className="profile m-auto">
-          <Avatar
-            alt="José Delgado"
-            src={Profile}
-            sx={{ width: 128, height: 128 }}
-          />
-        </div>
-        <div className="resume mt-4">
-          <h1 className="font-black">José Delgado</h1>
-          <p>Software Developer</p>
-          <div className="location flex mt-8"> <span><img className="w-4 h-4" src={Location} /></span>Lima, Perú</div>
-          <div className="social flex">
-            <a href="https://github.com/jDebu" target="_blank"><img src={Github} className="w-4 h-4 mt-2 mr-2"/></a>
-            <a href="https://www.linkedin.com/in/josedelgadobustamante/" target="_blank"><img src={Linkedin} className="w-4 h-4 mt-2"/></a>
-          </div>
+      <Header />
+      <div className="block">
+        <div className="flex justify-evenly">
+          <main className="mt-4 flex-auto">
+            <Container className="px-5 md:px-20 flex items-center">
+              <div className="profile-header py-3">
+                <h1 className="font-black">José Delgado</h1>
+              </div>
+            </Container>
+            <Routes>
+              <Route
+                path="/*"
+                element={
+                  <PublicRoute redirect={"/"}>
+                    <HomeRoutes />
+                  </PublicRoute>
+                }
+              />
+            </Routes>
+          </main>
+          <aside className="mt-4 ml-2">
+            <div>
+              <div className="profile m-auto">
+                <Avatar
+                  alt="José Delgado"
+                  src={Profile}
+                  sx={{ width: 128, height: 128 }}
+                />
+              </div>
+              <div className="resume mt-4">
+                <h1 className="font-black">José Delgado</h1>
+                <p>Software Developer</p>
+                <div className="location flex mt-8">
+                  {" "}
+                  <span>
+                    <img className="w-4 h-4" src={Location} />
+                  </span>
+                  Lima, Perú
+                </div>
+                <div className="social flex">
+                  <a href="https://github.com/jDebu" target="_blank">
+                    <img src={Github} className="w-4 h-4 mt-2 mr-2" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/josedelgadobustamante/"
+                    target="_blank"
+                  >
+                    <img src={Linkedin} className="w-4 h-4 mt-2" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </aside>
         </div>
       </div>
-    </aside>
-    </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
